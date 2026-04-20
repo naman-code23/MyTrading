13e8d0840ce163fa7834329a3aa1e3485cac5175
naman-code23 <70807938+naman-code23@users.noreply.github.com>
2026-04-20 21:35:00 +0530

    Update config.js

--- trademaster-web-v4/js/config.js
+++ trademaster-web-v4/js/config.js
@@ -1,10 +1,11 @@
 window.TRADEMASTER_CONFIG = {
   firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT.appspot.com',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID'
+    apiKey: "AIzaSyC3MO3DDq593kQva2zGZPq1ukvxo0dWNCs",
+    authDomain: "trading-d5a0e.firebaseapp.com",
+    projectId: "trading-d5a0e",
+    storageBucket: "trading-d5a0e.firebasestorage.app",
+    messagingSenderId: "964981936115",
+    appId: "1:964981936115:web:cca20788fa23f41591cc86",
+    measurementId: "G-WH2S5GJEZL"
   }
 };

36e674ddd7399786a6a722140896ec3745806fda
naman-code23 <70807938+naman-code23@users.noreply.github.com>
2026-04-21 12:07:15 +0530

    Update config.js

--- trademaster-web-v9-winner-fixes/js/config.js
+++ trademaster-web-v9-winner-fixes/js/config.js
@@ -1,13 +1,11 @@
 window.TRADEMASTER_CONFIG = {
   firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    // Use the exact bucket value shown in Firebase Console.
-    // New projects usually use YOUR_PROJECT.firebasestorage.app
-    // Older projects may use YOUR_PROJECT.appspot.com
-    storageBucket: 'YOUR_STORAGE_BUCKET',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID'
+    apiKey: "AIzaSyC3MO3DDq593kQva2zGZPq1ukvxo0dWNCs",
+    authDomain: "trading-d5a0e.firebaseapp.com",
+    projectId: "trading-d5a0e",
+    storageBucket: "trading-d5a0e.firebasestorage.app",
+    messagingSenderId: "964981936115",
+    appId: "1:964981936115:web:cca20788fa23f41591cc86",
+    measurementId: "G-WH2S5GJEZL"
   }
 };
